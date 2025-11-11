94f02ece1f7a32d7438188e04d308f84c7c1ae47
Your Name <your-email@example.com>
2025-11-11 19:02:18 +0530

    up p

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -1,5 +1,10 @@
 import { test, expect } from '@playwright/test';
 
+test.use({
+  screenshot: 'on', 
+  video: 'on',
+ })
+
 test('test', async ({ page }) => {
   await page.goto('https://www.acko.com/');
 
@@ -7,5 +12,8 @@ test('test', async ({ page }) => {
   await page.getByRole('textbox',{name:"e.g. MH05VF1234"}).click();  
   await page.getByRole('textbox',{name:"e.g. MH05VF1234"}).fill("mh14lj1234");  
   await page.getByRole('button',{name:"Check ACKO price"}).click();
+
+    await page.screenshot({ path: 'acko-bike-test.png', fullPage: true });
+
   
 });
